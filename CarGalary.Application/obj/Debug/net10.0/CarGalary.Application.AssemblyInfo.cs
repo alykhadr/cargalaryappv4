4bd8b61213dd7eab265631aca124fb059679d31a
alykhadrr <alikhadr22@outlook.com>
2026-02-14 16:05:31 +0300

    ADD NEW APP

--- CarGalary.Application/obj/Debug/net10.0/CarGalary.Application.AssemblyInfo.cs
+++ CarGalary.Application/obj/Debug/net10.0/CarGalary.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarGalary.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb9b8ad2e50798a14521dbe49a475dc4b39573d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efa1a1b077af1a2459911b77dcd6f7c308db85e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarGalary.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarGalary.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
