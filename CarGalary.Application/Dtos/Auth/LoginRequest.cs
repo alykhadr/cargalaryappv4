ebdf0fd03d7868bbde3b20075ee2e95e95919ae0
alykhadrr <alikhadr22@outlook.com>
2026-02-22 16:27:42 +0300

    add remember me

--- CarGalary.Application/Dtos/Auth/LoginRequest.cs
+++ CarGalary.Application/Dtos/Auth/LoginRequest.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CarGalary.Application.Dtos.Auth
 {
    public class LoginRequest
     {
-        public string UserName { get; set; } = null!;// may be email or user name
+        public string UserName { get; set; } = null!; // may be email or user name
         public string Password { get; set; } = null!;
+        public bool RememberMe { get; set; }
     }
 }
